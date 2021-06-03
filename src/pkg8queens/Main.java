d02a1649bcd7623cc76a0e34cc2b32591d1d6348
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2021-06-03 10:54:11 -0400

    Minor changes

--- src/pkg8queens/Main.java
+++ src/pkg8queens/Main.java
@@ -11,7 +11,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Queens game = new Queens();
+        Queens game = new Queens(20);
         game.train();
     }
 }
